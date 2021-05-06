e0788d1d1c40eb2fbc1c38d4436951228540efdb
mniepytalski <mniepytalski@gmail.com>
2021-05-06 11:19:20 +0200

    FIX player model

--- src/main/java/pl/cbr/games/snake/player/PlayerModel.java
+++ src/main/java/pl/cbr/games/snake/player/PlayerModel.java
@@ -30,21 +30,21 @@ public class PlayerModel {
 
     public void move(MoveDirection direction) {
         if (direction == MoveDirection.LEFT) {
-            view.add(0, get(0).minus(new Point(gameConfig.getDotSize(),0)));
+            addHaed(getHead().minus(new Point(gameConfig.getDotSize(),0)));
         }
         if (direction==MoveDirection.RIGHT) {
-            view.add(0, get(0).add(new Point(gameConfig.getDotSize(),0)));
+            addHaed(getHead().add(new Point(gameConfig.getDotSize(),0)));
         }
         if (direction==MoveDirection.UP) {
-            view.add(0, get(0).minus(new Point(0,gameConfig.getDotSize())));
+            addHaed(getHead().minus(new Point(0,gameConfig.getDotSize())));
         }
         if (direction==MoveDirection.DOWN) {
-            view.add(0, get(0).add(new Point(0,gameConfig.getDotSize())));
+            addHaed(getHead().add(new Point(0,gameConfig.getDotSize())));
         }
-        limitLength();
+        limitTail();
     }
 
-    private void limitLength() {
+    private void limitTail() {
         if ( getLength() < view.size() ) {
             view.remove(view.size() - 1);
         }
@@ -56,7 +56,7 @@ public class PlayerModel {
 
     public boolean checkOurselfCollision() {
         for (int z = getViewSize()-1; z > 0; z--) {
-            if ((z > 4) && get(0).equals(get(z))) {
+            if ((z > 4) && getHead().equals(get(z))) {
                 return true;
             }
         }
@@ -64,7 +64,7 @@ public class PlayerModel {
     }
 
     public boolean isOutside(Rectangle boardModel) {
-        return boardModel.isOutside(get(0));
+        return boardModel.isOutside(getHead());
     }
 
     public void incLength() {
@@ -79,8 +79,16 @@ public class PlayerModel {
         return view.get(z);
     }
 
+    public Point getHead() {
+        return view.get(0);
+    }
+
     public void set(int z, Point point) {
         view.get(z).set(point);
+    }
+
+    private void addHaed(Point point) {
+        view.add(0,point);
     }
 
 }
